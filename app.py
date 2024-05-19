3cf118d27b3dd02b0359996607a5b5982d3f3072
Irrom <31070777+Bytestorm5@users.noreply.github.com>
2024-05-19 18:56:23 -0400

    notebooks!!!!!

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
+from werkzeug import exceptions as HTTPError
 import platform
+from nb import load_nb, get_nbs, get_nb_path
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -14,6 +17,27 @@ def projects():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html', notebooks=get_nbs())
+
+@app.route('/notebook/<nb>')
+def notebook(nb:str):
+    return render_template('notebook.html', notebook=load_nb(escape(nb)))
+
+@app.route('/download_notebook/<nb>')
+def dl_notebook(nb:str):
+    path = get_nb_path(escape(nb))
+    if not path:
+        raise HTTPError.NotFound("This notebook does not exist")
+    else:
+        return send_file(
+            path,
+            mimetype='application/octet-stream',
+            as_attachment=True,
+            download_name=f'{escape(nb)}.ipynb'
+        )
 
 if __name__ == '__main__':
     if platform.system() == 'Windows':
---
+++ nb.py
@@ -0,0 +1,44 @@
+from nbconvert import HTMLExporter
+import nbformat
+import os
+import time
+import pandas as pd
+
+nb_directory = pd.read_csv(os.path.join('notebooks', 'directory.csv'))
+
+def convert_notebook_to_html(notebook_path):
+    with open(notebook_path, encoding='utf-8') as f:
+        notebook = nbformat.read(f, as_version=4)
+    html_exporter = HTMLExporter()
+    (body, resources) = html_exporter.from_notebook_node(notebook)
+    return body
+
+def load_nb(name: str):
+    nb_path = os.path.join('notebooks', 'nbs', name + '.ipynb')    
+    
+    if not os.path.exists(nb_path):
+        raise FileNotFoundError(f"Notebook {name} not found.")
+    
+    cache_path = os.path.join('notebooks', 'cache', name + '.html')
+    
+    nb_last_modified = os.path.getmtime(nb_path)
+    
+    if os.path.exists(cache_path) and os.path.getmtime(cache_path) >= nb_last_modified:
+        with open(cache_path, 'r', encoding='utf-8') as f:
+            content = f.read()
+        return content
+    else:
+        html_content = convert_notebook_to_html(nb_path)
+        with open(cache_path, 'w', encoding='utf-8') as f:
+            f.write(html_content)
+        return html_content
+
+def get_nb_path(name):
+    nb_path = os.path.join('notebooks', 'nbs', name + '.ipynb')
+    if os.path.exists(nb_path):
+        return nb_path
+    else:
+        return False
+
+def get_nbs():
+    return nb_directory.to_numpy()
