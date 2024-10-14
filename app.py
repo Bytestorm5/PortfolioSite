5757a73fa6536607f988c53bfb4365994cec16b5
Irrom <31070777+Bytestorm5@users.noreply.github.com>
2024-10-14 16:34:56 -0400

    colorPicker
    picker
    
    guh
    
    Delete profiling_results.txt

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_file, redirect, url_for
+from flask import Flask, render_template, send_file, redirect, url_for, request
 from werkzeug import exceptions as HTTPError
 import platform
 from nb import load_nb, get_nbs, get_nb_path
@@ -53,6 +53,55 @@ def tools():
 @app.route('/tools/righthand_regex')
 def righthand_regex():
     return render_template('/tools/righthand_regex.html')
+
+@app.route('/tools/color_picker')
+def color_picker():
+    return render_template('/tools/color_picker.html')
+
+from coloraide import Color
+import itertools
+import numpy as np
+from scipy.stats import norm
+
+@app.route('/tools_api/color_picker', methods = ["POST"])
+def color_picker_api():
+    colors_raw = request.json['colors']
+    colors = [Color(c).convert('oklch') for c in colors_raw]
+    
+    L_mean = np.mean([c._coords[0] for c in colors])
+    C_mean = np.mean([c._coords[1] for c in colors])
+    L_std = np.std([c._coords[0] for c in colors])
+    C_std = np.std([c._coords[1] for c in colors])
+    
+    for i in range(len(colors)):
+        for filter in ['protan', 'deutan', 'tritan']:
+            colors.append(colors[i].filter(filter).fit('srgb'))
+    
+    def objective(LCH):
+        test_color = Color('oklch', LCH).fit('srgb')
+        diff = min([test_color.delta_e(c, method='ok') for c in colors])
+        
+        L_prob = norm.pdf(LCH[0], L_mean, L_std)
+        C_prob = norm.pdf(LCH[1], C_mean, C_std)
+
+        # Combine the probabilities for weighting
+        weight = L_prob * C_prob
+
+        return diff - 5*(1 - weight)
+    
+    search_space = itertools.product(
+        np.linspace(0, 1, 5), # L
+        np.linspace(0, 0.37, 5), # C
+        np.linspace(0, 360, 36), # H        
+    )
+    
+    best_color = max(search_space, key=objective)
+    best_color_obj = Color('oklch', best_color)
+    return {
+        "color": best_color,
+        "color_hex": best_color_obj.convert('srgb').to_string(hex=True),
+        "fitness": f"{objective(best_color):.2f}"
+    }
 
 if __name__ == '__main__':
     if platform.system() == 'Windows':
