102bf9772b609d5c1232204458725f4daa257aa7
Irrom <31070777+Bytestorm5@users.noreply.github.com>
2024-10-14 16:34:56 -0400

    picker

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_file, redirect, url_for
+from flask import Flask, render_template, send_file, redirect, url_for, request
 from werkzeug import exceptions as HTTPError
 import platform
 from nb import load_nb, get_nbs, get_nb_path
@@ -53,6 +53,43 @@ def tools():
 @app.route('/tools/righthand_regex')
 def righthand_regex():
     return render_template('/tools/righthand_regex.html')
+
+@app.route('/tools/color_picker')
+def color_picker():
+    return render_template('/tools/color_picker.html')
+
+from coloraide import Color
+import itertools
+import numpy as np
+
+@app.route('/tools_api/color_picker', methods = ["POST"])
+def color_picker_api():
+    colors_raw = request.json['colors']
+    colors = [Color(c) for c in colors_raw]
+    
+    def color_diff(color1: Color, color2: Color):
+        diffs = [color1.delta_e(color2, method="ok")] # Normal Vision
+        for filter in ['protan', 'deutan', 'tritan']: # Diff Colorlindness Types
+            color1_t = color1.filter(filter)
+            color2_t = color2.filter(filter)
+            diffs.append(color1_t.delta_e(color2_t, method="ok"))
+        return min(diffs) / 100
+    
+    def objective(LCH):
+        test_color = Color('oklch', LCH).fit('srgb')
+        return min([color_diff(test_color, c) for c in colors])
+    
+    search_space = itertools.product(
+        np.linspace(0, 1, 10), # L
+        np.linspace(0, 0.37, 5), # C
+        np.linspace(0, 360, 36), # H        
+    )
+    
+    best_color = max(search_space, key=objective)
+    return {
+        "color": best_color,
+        "fitness": objective(best_color)
+    }
 
 if __name__ == '__main__':
     if platform.system() == 'Windows':
